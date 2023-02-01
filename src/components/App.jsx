431c5022fa8a860bf945256e86c7d8070996f8e9
jeliasalvaradoa <josealvarado30@hotmail.com>
2023-01-31 22:57:49 -0400

    Agrgando depentencias y configuraciones necesarias

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+
+return(
+    <div>
+<h1>hola Vida VIDA VIDA VIDA</h1>
+<h1>hola Vida VIDA VIDA VIDA</h1>
+<h1>hola Vida VIDA VIDA VIDA</h1>
+</div>
+);
+}
+
+
+export default App;
